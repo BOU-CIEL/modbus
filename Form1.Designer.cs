de662fd2357d532f881b6706ee7a0309c4eaafa2
adel <adelbouhafs621@gmail.com>
2025-10-16 11:11:52 +0200

    Commit #4

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,9 +30,12 @@
         {
             this.labelAdresseIP = new System.Windows.Forms.Label();
             this.textBoxAdressIP = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.buttonConnexion = new System.Windows.Forms.Button();
+            this.buttonDeconnexion = new System.Windows.Forms.Button();
+            this.textBoxStatut = new System.Windows.Forms.TextBox();
+            this.buttonLireTension = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxTension = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // labelAdresseIP
@@ -49,45 +52,74 @@
             this.textBoxAdressIP.Location = new System.Drawing.Point(112, 40);
             this.textBoxAdressIP.Name = "textBoxAdressIP";
             this.textBoxAdressIP.Size = new System.Drawing.Size(144, 20);
-            this.textBoxAdressIP.TabIndex = 1;
-
+            const int V = 1;
+            this.textBoxAdressIP.TabIndex = V;
             // 
-            // button1
+            // buttonConnexion
             // 
-            this.button1.Location = new System.Drawing.Point(353, 40);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Connexion";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.buttonConnexion.Location = new System.Drawing.Point(353, 40);
+            this.buttonConnexion.Name = "buttonConnexion";
+            this.buttonConnexion.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnexion.TabIndex = 2;
+            this.buttonConnexion.Text = "Connexion";
+            this.buttonConnexion.UseVisualStyleBackColor = true;
+            this.buttonConnexion.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // buttonDeconnexion
             // 
-            this.button2.Location = new System.Drawing.Point(450, 40);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(89, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Deconnexion";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.buttonDeconnexion.Location = new System.Drawing.Point(450, 40);
+            this.buttonDeconnexion.Name = "buttonDeconnexion";
+            this.buttonDeconnexion.Size = new System.Drawing.Size(89, 23);
+            this.buttonDeconnexion.TabIndex = 3;
+            this.buttonDeconnexion.Text = "Deconnexion";
+            this.buttonDeconnexion.UseVisualStyleBackColor = true;
+            this.buttonDeconnexion.Click += new System.EventHandler(this.button2_Click);
             // 
-            // textBox2
+            // textBoxStatut
             // 
-            this.textBox2.Location = new System.Drawing.Point(570, 40);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(197, 389);
-            this.textBox2.TabIndex = 4;
+            this.textBoxStatut.Location = new System.Drawing.Point(570, 40);
+            this.textBoxStatut.Multiline = true;
+            this.textBoxStatut.Name = "textBoxStatut";
+            this.textBoxStatut.Size = new System.Drawing.Size(197, 389);
+            this.textBoxStatut.TabIndex = 4;
+            // 
+            // buttonLireTension
+            // 
+            this.buttonLireTension.Location = new System.Drawing.Point(353, 127);
+            this.buttonLireTension.Name = "buttonLireTension";
+            this.buttonLireTension.Size = new System.Drawing.Size(75, 23);
+            this.buttonLireTension.TabIndex = 5;
+            this.buttonLireTension.Text = "Lire";
+            this.buttonLireTension.UseVisualStyleBackColor = true;
+            this.buttonLireTension.Click += new System.EventHandler(this.buttonLireTension_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(353, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Tension moteur en Volt";
+            // 
+            // textBoxTension
+            // 
+            this.textBoxTension.Location = new System.Drawing.Point(450, 129);
+            this.textBoxTension.Name = "textBoxTension";
+            this.textBoxTension.Size = new System.Drawing.Size(89, 20);
+            this.textBoxTension.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxTension);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonLireTension);
+            this.Controls.Add(this.textBoxStatut);
+            this.Controls.Add(this.buttonDeconnexion);
+            this.Controls.Add(this.buttonConnexion);
             this.Controls.Add(this.textBoxAdressIP);
             this.Controls.Add(this.labelAdresseIP);
             this.Name = "Form1";
@@ -101,9 +133,12 @@
 
         private System.Windows.Forms.Label labelAdresseIP;
         private System.Windows.Forms.TextBox textBoxAdressIP;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button buttonConnexion;
+        private System.Windows.Forms.Button buttonDeconnexion;
+        private System.Windows.Forms.TextBox textBoxStatut;
+        private System.Windows.Forms.Button buttonLireTension;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxTension;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using modbus;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,36 +16,40 @@ namespace TP_Modbus
 {
     public partial class Form1 : Form
     {
+        private CModbus modbus;
         public Form1()
         {
             InitializeComponent();
+            modbus = new CModbus();
         }
         private Socket socket;
+        private byte[] trameE;
+        private byte[] trameR;
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            
+
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {   
+        {
             if (textBoxAdressIP.TextLength > 6)
             {
                 string TextAdressIP = textBoxAdressIP.Text;
-                textBox2.AppendText("Connexion au serveur 172.17.50.180 \n");
+                textBoxStatut.AppendText("Connexion au serveur 172.17.50.180 \n");
                 this.socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
                 IPEndPoint iped = new IPEndPoint(IPAddress.Parse(TextAdressIP), 502);
                 try
                 {
                     this.socket.Connect(iped);
-                    textBox2.AppendText("Connexion ok \r\n");
+                    textBoxStatut.AppendText("Connexion ok \r\n");
                 }
-                catch(System.Exception ex) 
+                catch (System.Exception ex)
                 {
-                    this.textBox2.AppendText("**Exception : Impossible de se connecter au serveur" + ex.Message + "\r\n");
+                    this.textBoxStatut.AppendText("**Exception : Impossible de se connecter au serveur" + ex.Message + "\r\n");
                 }
             }
-            
+
 
         }
 
@@ -54,12 +59,24 @@ namespace TP_Modbus
             try
             {
                 this.socket.Close();
-                this.textBox2.AppendText("Deconnexion reussie \r\n");
+                this.textBoxStatut.AppendText("Deconnexion reussie \r\n");
             }
             catch (System.Exception ex)
             {
-                this.textBox2.AppendText("Deconnexion impossible" + ex.Message + "\r\n");
+                this.textBoxStatut.AppendText("Deconnexion impossible" + ex.Message + "\r\n");
             }
+        }
+
+        private void buttonLireTension_Click(object sender, EventArgs e)
+        {
+            var trameE = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x05, 0x01, 0x03, 0x02, 0x08, 0x93 };
+            this.socket.Send(trameE);
+            var trameR = new byte[256];
+            int nbBytes = this.socket.Receive(trameR);
+            int tensionRaw = (trameR[9] << 8) | trameR[10];
+            double tension = tensionRaw / 10.0;
+            this.textBoxTension.Text = nbBytes.ToString();
         }
     }
 }
+  
